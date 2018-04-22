5f82588d63222e8debd70826784254962f0d04b6
vulgo <imvulgo@gmail.com>
2018-04-22 20:16:48 +0100

    Silence some warnings

--- WebDriverManager/Scripts/component.sh
+++ WebDriverManager/Scripts/component.sh
@@ -4,12 +4,11 @@
 # $2: Drivers component directory name (from distribution.xml)
 # $3: New drivers component pkg filename (output)
 
-cd $1/..
+cd "$1/.."
 gunzip -dc < "$1/$2/Payload" > "$1/../payload.cpio"
 mkdir drivers-root
-cd drivers-root
-cpio -i < "$1/../payload.cpio"
-cd ..
+( cd drivers-root
+cpio -i < "$1/../payload.cpio" )
 pkgbuild --root drivers-root --scripts "$1/$2/Scripts" --identifier com.nvidia.web-driver "$3"
 rm payload.cpio
 rm -rf drivers-root
