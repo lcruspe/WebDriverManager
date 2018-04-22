8fadfc46b058f153366ffc63957ecd4eb5239950
vulgo <imvulgo@gmail.com>
2018-04-22 07:54:19 +0100

    A new image for the About... window

--- WebDriverManager/BusyViewController.swift
+++ WebDriverManager/BusyViewController.swift
@@ -1,10 +1,21 @@
-//
-//  BusyViewController.swift
-//  Web Driver Manager
-//
-//  Created by Mark on 21/04/2018.
-//  Copyright © 2018 vulgo. All rights reserved.
-//
+/*
+ * File: BusyViewController.swift
+ *
+ * WebDriverManager © vulgo 2018
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 import Cocoa
 
