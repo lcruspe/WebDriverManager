13f02c66021d99aa348888db5532243fabe0989b
vulgo <imvulgo@gmail.com>
2018-04-27 08:11:03 +0100

    Fix packager bug where installer .pl scripts were not properly packaged

--- WebDriverManager/Scripts/component.sh
+++ WebDriverManager/Scripts/component.sh
@@ -5,10 +5,24 @@
 # $3: New drivers component pkg filename (output)
 
 cd "$1/.."
+
+# Payload
 gunzip -dc < "$1/$2/Payload" > "$1/../payload.cpio"
 mkdir drivers-root
 ( cd drivers-root
 cpio -i < "$1/../payload.cpio" )
-pkgbuild --root drivers-root --scripts "$1/$2/Scripts" --identifier com.nvidia.web-driver "$3"
+
+# Scripts
+gunzip -dc < "$1/$2/Scripts" > "$1/../scripts.cpio"
+mkdir scripts
+( cd scripts
+cpio -i < "$1/../scripts.cpio" )
+
+# Build
+pkgbuild --root drivers-root --scripts scripts --identifier com.nvidia.web-driver "$3"
+
+# Clean up
 rm payload.cpio
+rm scripts.cpio
 rm -rf drivers-root
+rm -rf scripts
